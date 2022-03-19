c2e711adf46431c8f3fcdf6ea0d03dc379fd1995
wanjiku <annritawanjiku77@gmail.com>
2022-03-19 19:03:18 +0300

    add full name

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 //contactconstructor-backend
 function Contact(first, last) {
-    this.firstName=first;
-    this.lastName=last;
+  this.firstName = first;
+  this.lastName = last;
 }
+// add full names
+Contact.prototype.fullName = function () {
+  return this.firstName + "" + this.lastName;
+};
 
-// user inerface logic/front end
-$(document).ready(function(){
-    $("form#new-contact").submit(function(event){
-        event.preventDefault()
+// user inerface logic/front end -jquery
+$(document).ready(function () {
+  $("form#new-contact").submit(function (event) {
+    event.preventDefault();
 
-        var inputtedFirstName=$("input#new-first-name").val()
-        var inputtedLastName=$("input#new-last-name").val()
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
 
-        var newContact= new Contact(inputtedFirstName,inputtedLastName)
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName +"</span></li>")
-        
-        $("input#new-first-name").val()
-        $("input#new-last-name").val()
-    })
-})
+    $("ul#contacts").append(
+      "<li><span class='contact'>" + newContact.fullName() + "</span></li>"
+    );
+
+    $("input#new-first-name").val();
+    $("input#new-last-name").val();
+
+    $(".contact")
+      .last()
+      .click(function () {
+        $("#show-contact").show();
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+      });
+  });
+});
